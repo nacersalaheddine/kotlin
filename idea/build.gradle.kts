2a57aac8664d3f3b9b294e2b4b675294d4c5cecf
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-06-01 16:00:55 +0300

    -- use Jre for now because of KT-24730

--- idea/build.gradle.kts
+++ idea/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 dependencies {
     testRuntime(intellijDep())
 
-    compile(projectDist(":kotlin-stdlib-jdk8"))
+    compile(projectDist(":kotlin-stdlib-jre8"))
     compileOnly(project(":kotlin-reflect-api"))
     compile(project(":core:descriptors"))
     compile(project(":core:descriptors.jvm"))
